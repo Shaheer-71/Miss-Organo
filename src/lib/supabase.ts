114076fbb7236299ec69bd8c23d823a05c9a163e
shaheer-71 <devshaheer360@gmail.com>
2025-06-06 19:57:57 +0500

    test

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,13 +2,13 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabase';
 
 
-console.log('VITE_HOST:', import.meta.env.VITE_HOST);
-console.log('key:', import.meta.env.VITE_KEY);
-console.log('cloud:', import.meta.env.VITE_CLOUD);
-console.log('preset:', import.meta.env.VITE_PRESET);
+console.log('host:', import.meta.env.host);
+console.log('key:', import.meta.env.key);
+console.log('cloud:', import.meta.env.cloud);
+console.log('preset:', import.meta.env.preset);
 
 
-const supabaseUrl = import.meta.env.VITE_HOST || 'https://kubrirjahikgpwfzsepo.supabase.co';
+const supabaseUrl = import.meta.env.host || 'https://kubrirjahikgpwfzsepo.supabase.co';
 const supabaseAnonKey = import.meta.env.key || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1YnJpcmphaGlrZ3B3ZnpzZXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1OTg0ODQsImV4cCI6MjA2NDE3NDQ4NH0.KlEmBfT1_-M8lxp2GQer5v6e3iGQZTmyEU0E4U04A-0';
 
 if (!supabaseUrl || !supabaseAnonKey) {
